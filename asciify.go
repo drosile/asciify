38a45824aa3621886b704f1fa53afe6f04137c25
Dave Rosile <drosile@gmail.com>
2017-04-12 21:17:41 -0600

    Separate reading image data from asciify_handler

--- asciify.go
+++ asciify.go
@@ -2,19 +2,48 @@ package main
 
 import (
   "fmt"
+  "html"
+  "io/ioutil"
   "net/http"
 )
 
-func asciify_handler(w http.ResponseWriter, r *http.Request) {
+func read_image_data(r *http.Request) (image_data []byte, err error) {
   const (
-    test_image = "http://i.imgur.com/ewD2qvQ.jpg"
+    test_image_url = "http://i.imgur.com/ewD2qvQ.jpg"
   )
-  resp, err := http.Get(test_image)
+  r.ParseForm()
+  image_url := html.UnescapeString(r.FormValue("image_url"))
+
+  // TODO: add image upload support
+  if image_data == nil {
+    if image_url == "" {
+      image_url = test_image_url
+    }
+
+    resp, image_err := http.Get(image_url)
+    // FIXME: there must be a more idiomatic way to handle errors
+    if image_err != nil {
+      err = image_err
+      return
+    }
+    i, read_err := ioutil.ReadAll(resp.Body)
+    if read_err != nil {
+      err = read_err
+      return
+    }
+    image_data = i
+  }
+  return
+}
+
+func asciify_handler(w http.ResponseWriter, r *http.Request) {
+  image_data, err := read_image_data(r)
   if err != nil {
     fmt.Fprintf(w, "request failed")
     return
   }
-  fmt.Fprintf(w, "request succeeded %s", resp)
+
+  fmt.Fprintf(w, "%s", image_data)
 }
 
 func main() {
